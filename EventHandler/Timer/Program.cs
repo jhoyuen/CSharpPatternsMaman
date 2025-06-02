1026cf0d9cb49c3a05bd76c6385057a16bebd2ab
jhoyuen <jon_lg83@yahoo.com>
2025-06-02 20:40:18 +1000

    JCPM-2 Add Event Handler example Timer (#2)
    Co-authored-by: Zenyno <jonathan.klcflam@gmail.com>

---
+++ EventHandler/Timer/Program.cs
@@ -0,0 +1,9 @@
+﻿using Timer;
+
+var timer = new Timer.Timer();
+var alarm = new Alarm();
+
+alarm.Subscribe(timer);
+timer.Start(10000);
+
+Console.Read();
---
+++ EventHandler/Timer/Timer.cs
@@ -0,0 +1,20 @@
+﻿namespace Timer;
+
+public class Timer
+{
+    public event EventHandler<TimeElapsedEventArgs>? TimeElapsed;
+
+    public void Start(int milliseconds)
+    {
+        Console.WriteLine("Timer started.");
+        Console.WriteLine($"It is currently {DateTime.Now}");
+        System.Threading.Thread.Sleep(milliseconds);
+
+        OnTimeElapsed(new TimeElapsedEventArgs(DateTime.Now));
+    }
+
+    protected virtual void OnTimeElapsed(TimeElapsedEventArgs e)
+    {
+        TimeElapsed?.Invoke(this, e);
+    }
+}
