f2233c663cdce2e5062e7cf29f4837d5ad9efb85
jhoyuen <jon_lg83@yahoo.com>
2025-06-12 17:44:59 +1000

    JCPM-4 Add Event Handler project for new console App MultiplayerVersus (#4)
    Co-authored-by: Jonathan Lam <jonathan.lam@innerrange.com>

---
+++ EventHandler/MultiplayerVersus/Player.cs
@@ -0,0 +1,39 @@
+﻿namespace MultiplayerVersus;
+
+public class Player
+{
+    public string Name { get; set; }
+    public int HP { get; private set; }
+    public bool IsAlive
+    {
+        get
+        {
+            return HP > 0;
+        }
+    }
+
+    public event EventHandler<PlayerDamageEventArgs> TookDamage;
+    public event EventHandler Defeated;
+
+    public Player(string name, int initialHP)
+    {
+        Name = name;
+        HP = initialHP;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if (!IsAlive)
+            return;
+
+        HP -= damage;
+        HP = Math.Max(HP, 0);
+
+        TookDamage?.Invoke(this, new PlayerDamageEventArgs(damage, HP));
+
+        if(HP <= 0)
+        {
+            Defeated?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ EventHandler/MultiplayerVersus/PlayerDamageEventArgs.cs
@@ -0,0 +1,13 @@
+﻿namespace MultiplayerVersus;
+
+public class PlayerDamageEventArgs :EventArgs
+{
+    public int Damage { get; set; }
+    public int RemainingHP { get; set; }
+
+    public PlayerDamageEventArgs(int damage, int remainingHP)
+    {
+        Damage = damage;
+        RemainingHP = remainingHP;
+    }
+}
